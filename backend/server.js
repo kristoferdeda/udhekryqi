39502542b154ceaa88813b27d54c1758e2cea427
kristoferdeda <kristi.deda15@gmail.com>
2025-06-09 12:52:01 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ app.use(cors());
 app.use(express.json()); // Parse JSON bodies
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('✅ Connected to MongoDB');
   // Start the server only after DB is connected
